80b96b1a42fbf08a9e29d69297aa77de0cddbd33
Marcus Sky <astronautmarkus@gmail.com>
2023-10-09 22:18:03 -0300

    eliminado los menús por fuerza mayor.

--- RepartidorV2/app-repartidor-v2/src/app/components/components.module.ts
+++ RepartidorV2/app-repartidor-v2/src/app/components/components.module.ts
@@ -2,18 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
-import { HeaderLoggedInComponent } from './header-logged-in/header-logged-in.component';
-
-import { SidemenuComponent } from './sidemenu/sidemenu.component';
-
-import { SidemenuLoggedInComponent } from './sidemenu-logged-in/sidemenu-logged-in.component';
-
 import { IonicModule } from '@ionic/angular';
 
 
 @NgModule({
-  declarations: [HeaderComponent,HeaderLoggedInComponent,SidemenuComponent, SidemenuLoggedInComponent],
-  exports:[HeaderComponent,HeaderLoggedInComponent,SidemenuComponent, SidemenuLoggedInComponent],
+  declarations: [HeaderComponent,],
+  exports:[HeaderComponent,],
   imports: [
     CommonModule,
     IonicModule,
--- RepartidorV2/app-repartidor-v2/src/app/components/header/header.component.ts
+++ RepartidorV2/app-repartidor-v2/src/app/components/header/header.component.ts
@@ -10,7 +10,6 @@ export class HeaderComponent  implements OnInit {
 // Input permite recibir la variable 
   //          por parámetro desde el Html
   @Input() titulo:string = 'Camellos Food'  //<==========
-  @Input() showButton = true; // Controla la visibilidad del botón
 
   constructor() { }
 
